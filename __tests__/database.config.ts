bb13d14199182067ad8c375698e0fa5f63271486
argas7 <argas@cin.ufpe.br>
2022-01-21 01:32:39 -0300

    fix: upgrade database test connection

--- __tests__/database.config.ts
+++ __tests__/database.config.ts
@@ -1,4 +1,4 @@
-import { createConnection, getConnection, ConnectionOptions } from 'typeorm';
+import { createConnection, getConnection, Connection, ConnectionOptions } from 'typeorm';
 import 'dotenv/config';
 
 import { User } from '../src/db/models';
@@ -19,23 +19,46 @@ const options: ConnectionOptions = {
   cli: { entitiesDir: '../src/db/models' },
 };
 
+const getNewConnection = async (connectionOptions: ConnectionOptions): Promise<Connection> => {
+  try {
+    const connection = await createConnection(connectionOptions);
+
+    console.log('Successfully connected to postgres!');
+    return connection;
+  } catch (error) {
+    console.log('Error: could not connect to postgres!');
+    throw new Error(error);
+  }
+};
+
+export const TypeormHelper = {
+  connection: null as Connection,
+  async connect(): Promise<Connection> {
+    const connection = await getNewConnection(options);
+    await connection.synchronize();
+    this.connection = connection;
+
+    return this.connection;
+  },
+};
+
 const connection = {
   async create() {
-    await createConnection(options);
+    await TypeormHelper.connect();
   },
 
   async close() {
-    await getConnection().close();
+    await TypeormHelper.connection.close();
   },
 
   async clear() {
-    const currConnection = getConnection();
+    const currConnection = TypeormHelper.connection;
     const entities = currConnection.entityMetadatas;
 
     await Promise.all(
       entities.map(async (entity) => {
         const repository = currConnection.getRepository(entity.name);
-        await repository.query(`DELETE FROM ${entity.tableName}`);
+        await repository.query(`DELETE FROM "${entity.tableName}"`);
       }),
     );
   },
