de018ce140d9b54f72618e9e709f5bc4dc067163
argas7 <argas@cin.ufpe.br>
2022-01-20 21:04:20 -0300

    test: create my first test file

---
+++ __tests__/unit/sum.spec.ts
@@ -0,0 +1,10 @@
+describe('First Test', () => {
+  it('should sum two numbers', () => {
+    const a = 10;
+    const b = 20;
+
+    const result = a + b;
+
+    expect(result).toBe(30);
+  });
+});
