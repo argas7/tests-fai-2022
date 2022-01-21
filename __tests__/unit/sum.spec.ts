090f68845baafc02e7d3a1eec2809e5d15953a5b
argas7 <argas@cin.ufpe.br>
2022-01-20 21:28:45 -0300

    test: clean up tests display

--- __tests__/unit/sum.spec.ts
+++ __tests__/unit/sum.spec.ts
@@ -1,56 +1,62 @@
-describe('Firsts Test', () => {
-  beforeAll(() => {
-    console.log('########## I was called before start a block of tests ###########');
-  });
-
-  beforeEach(() => {
-    console.log('===> After that, a test will begin');
-  });
-
-  afterEach(() => {
-    console.log('===> A test was finished right now');
-  });
-
-  afterAll(() => {
-    console.log('####### I was called after finish a entire block of tests #######');
-  });
-
-  it('should sum two numbers', () => {
-    console.log('# This is being called inside a test');
-    const a = 10;
-    const b = 20;
-
-    const result = a + b;
-
-    expect(result).toBe(30);
-  });
-
-  it('should test a string value', () => {
-    console.log('# This is being called inside a test');
-    const myString = 'Hello world';
-
-    expect(myString).toBe('Hello world');
-    expect(myString).toHaveLength(11);
-    expect(myString).toBeTruthy();
-    expect(myString.split(' ')).toHaveLength(2);
-  });
-
-  it('show compare properties from two objects', () => {
-    console.log('# This is being called inside a test');
-    const result = {
-      a: 'this',
-      b: 'is',
-      c: 'one',
-      d: 'test',
-      e: 'example',
-    };
-
-    expect(result).toEqual({
-      a: 'this',
-      b: 'is',
-      c: 'one',
-      d: 'test',
-      e: 'example'
-    });
-  });
+describe('Firsts Tests', () => {
+  test.todo('should sum two numbers');
+  test.todo('should test a string value');
+  test.todo('should compare properties from two objects');
 });
+
+// describe('Firsts Test', () => {
+//   beforeAll(() => {
+//     console.log('########## I was called before start a block of tests ###########');
+//   });
+
+//   beforeEach(() => {
+//     console.log('===> After that, a test will begin');
+//   });
+
+//   afterEach(() => {
+//     console.log('===> A test was finished right now');
+//   });
+
+//   afterAll(() => {
+//     console.log('####### I was called after finish a entire block of tests #######');
+//   });
+
+//   it('should sum two numbers', () => {
+//     console.log('# This is being called inside a test');
+//     const a = 10;
+//     const b = 20;
+
+//     const result = a + b;
+
+//     expect(result).toBe(30);
+//   });
+
+//   it('should test a string value', () => {
+//     console.log('# This is being called inside a test');
+//     const myString = 'Hello world';
+
+//     expect(myString).toBe('Hello world');
+//     expect(myString).toHaveLength(11);
+//     expect(myString).toBeTruthy();
+//     expect(myString.split(' ')).toHaveLength(2);
+//   });
+
+//   it('show compare properties from two objects', () => {
+//     console.log('# This is being called inside a test');
+//     const result = {
+//       a: 'this',
+//       b: 'is',
+//       c: 'one',
+//       d: 'test',
+//       e: 'example',
+//     };
+
+//     expect(result).toEqual({
+//       a: 'this',
+//       b: 'is',
+//       c: 'one',
+//       d: 'test',
+//       e: 'example',
+//     });
+//   });
+// });
