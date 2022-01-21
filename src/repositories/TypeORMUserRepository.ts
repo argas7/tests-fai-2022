c01ccd767e5ebc746a0c45b6d5663efe48a7a479
argas7 <argas@cin.ufpe.br>
2022-01-20 23:35:04 -0300

    feat: change repository to really create user at database

--- src/repositories/TypeORMUserRepository.ts
+++ src/repositories/TypeORMUserRepository.ts
@@ -1,10 +1,15 @@
 import { UserRepository } from '@/data/repositories';
+import { UserUsecase } from '@/data/usecases';
 import { TypeormHelper } from '@/db/config';
 import { User } from '@/db/models';
 
 export class TypeORMUserRepository implements UserRepository {
-  create() {
-    return { id: 'as', name: 'Edgar', age: 19, email: 'edgar@marques.com', password: 'senha123' };
+  async create(createParams: UserUsecase.createParams) {
+    const user = await TypeormHelper.connection.getRepository(User).save({
+      ...createParams,
+    });
+
+    return user;
   }
 
   async list() {
